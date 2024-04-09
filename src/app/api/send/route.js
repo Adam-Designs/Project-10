8250b0796390bc4081bb1ea255122a0a24b06e14
R-Designs <intelligentvii@gmail.com>
2024-04-09 18:03:29 -0500

    Update route.js

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.re_77oLTzLb_AUryrxgiGBo2dy5ifpkFbKBj);
+const resend = new Resend(re_77oLTzLb_AUryrxgiGBo2dy5ifpkFbKBj);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
